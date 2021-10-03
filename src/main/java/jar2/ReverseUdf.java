7dfe34b28fc84acf17d5795c507be618303bf059
nani <nani@mycom.com>
2021-10-03 22:26:13 +0530

    done by jar2

---
+++ src/main/java/jar2/ReverseUdf.java
@@ -0,0 +1,28 @@
+package jar2;
+
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+
+@UdfDescription(name = "reverse", description = "Example UDF that reverses an object")
+public class ReverseUdf {
+
+  @Udf(description = "Reverse a string")
+  public String reverseString(String source) {
+    return new StringBuilder(source).reverse().toString();
+  }
+
+  @Udf(description = "Reverse an integer")
+  public String reverseInt(Integer source) {
+    return new StringBuilder(source.toString()).reverse().toString();
+  }
+
+  @Udf(description = "Reverse a long")
+  public String reverseLong(Long source) {
+    return new StringBuilder(source.toString()).reverse().toString();
+  }
+
+  @Udf(description = "Reverse a double")
+  public String reverseDouble(Double source) {
+    return new StringBuilder(source.toString()).reverse().toString();
+  }
+}
